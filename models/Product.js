0893f9182d9ba9138dad6069437b41dbf90395db
[Wyatt Drew] <Wyatt.J.Drew@gmail.com>
2025-02-01 12:12:54 -0600

    schema update!

--- models/Product.js
+++ models/Product.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
-    product_num: String,
-    product_name: String,
+    product_num: { type: String, required: true },
+    product_name: { type: String, required: true },
+    parent_company: { type: String }, // Add this
+    product_brand: { type: String },
+    product_link: { type: String },
+    image_url: { type: String },
+    description: { type: String },
+    search_terms: { type: [String] } // Add search terms
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
